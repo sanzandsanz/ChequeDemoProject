bb0ee2d5ed62136e8480cfea44d8651a3fb69d80
sanzandsanz <sanjay.bajracharya4@gmail.com>
2017-06-29 10:01:42 +1000

    Final check in for cheque project

---
+++ Deepend/DeependCheque/Controllers/ChequeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Mvc;
+using DeependCheque.Models;
+
+namespace DeependCheque.Controllers
+{
+    public class ChequeController : Controller
+    {
+
+        /// <summary>
+        ///  Get all the cheques
+        /// </summary>
+        /// <returns>All cheques </returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            using (var client = new HttpClient())
+            {
+                string baseAddress = ConfigurationManager.AppSettings["BaseAddress"];
+                client.BaseAddress = new Uri(baseAddress);
+
+                HttpResponseMessage response = client.GetAsync("api/chequeservice").Result;
+                if (!response.IsSuccessStatusCode) // ? Error exist 
+                {
+                    var errorMesage = string.Format("Error Status Code: {0}, Error Message: {1}", (int)response.StatusCode,
+                                                                                       response.Content.ReadAsStringAsync().Result);
+                    throw new Exception(errorMesage);
+                }
+
+                var cheques = response.Content.ReadAsAsync<List<Cheque>>().Result;
+                cheques = cheques.OrderByDescending(i => i.Date).ToList();
+
+                return this.View(cheques);
+            }
+        }
+
+
+        /// <summary>
+        /// Get the check details 
+        /// </summary>
+        /// <param name="id">Cheque Id</param>
+        /// <returns>Cheque Details</returns>
+        [HttpGet]
+        public ActionResult GetCheckDetails(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                string baseAddress = ConfigurationManager.AppSettings["BaseAddress"];
+                client.BaseAddress = new Uri(baseAddress);
+
+                HttpResponseMessage response = client.GetAsync(string.Format("api/GetCheque/{0}", id)).Result;
+                if (!response.IsSuccessStatusCode) // Error Exist 
+                {
+                    var errorMesage = string.Format("Error Status Code: {0}, Error Message: {1}", (int)response.StatusCode,
+                                                                                     response.Content.ReadAsStringAsync().Result);
+                    throw new Exception(errorMesage);
+                }
+
+                var cheque = response.Content.ReadAsAsync<Cheque>().Result;
+                return this.View("Details", cheque);
+            }
+        }
+    }
+}
---
+++ Deepend/DeependCheque/Models/Cheque.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace DeependCheque.Models
+{
+    public class Cheque
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [RegularExpression(@"(\.\d{2}){1}$")]
+        public double Amount { get; set; }
+
+        public string Pay { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
